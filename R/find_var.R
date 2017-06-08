9308f939eda1fd93fd52970d40ab83d400c55940
Matthew Flickinger <mrflick@gmail.com>
2017-06-08 08:16:27 -0400

    Add find_var function

---
+++ R/find_var.R
@@ -0,0 +1,13 @@
+#' Find variables
+#'
+#' Search for a pattern in the OCSLS table column names
+#' @param x A pattern to search (grep) for
+#' @param tables A character vector of table names to search
+#'
+#' @export
+
+find_var <- function(x, tables=c("students","dates","romances","hookups")) {
+    sapply(tables, function(table) {
+        grep(x, names(get(table)), value=TRUE)
+    })
+}
